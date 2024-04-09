7d24aa75d2413aa36001e21f11de672f88e42d15
shaq31415926 <sarah.haq11@gmail.com>
2024-04-09 13:15:34 +0200

    code for stream a

--- tech_basics_one/02Lecture/streamA_calculator.py
+++ tech_basics_one/02Lecture/streamA_calculator.py
@@ -3,13 +3,14 @@ num1 = input("Please select the first number:")
 operator = input("Please select an operator (+, -, *, /):")
 num2 = input("Please select the second number:")
 
-# converting the numbers to be floats, otherwise they default to style
+# converting the numbers to be floats, otherwise they default to string
 num1 = float(num1)
 num2 = float(num2)
 
+# we can print what the user enters
 print("The calculation is:", num1, operator, num2)
 
-# the if-elif statement
+# the if-elif-else statement that calculates the result
 if operator == "+":
     total = num1 + num2
 elif operator == '-':
@@ -21,5 +22,6 @@ elif operator == "/" and num2 > 0:
 else:
     total = "ERROR"
 
+# the final result
 # print("The total is", total)
-print(f"The total is {total}")
+print(f"The total is {total}") # the preferred way of printing the final out
