4ba390c5906aa105c539b3fdf725da9e7c3bc259
shaq31415926 <sarah.haq11@gmail.com>
2024-04-15 21:59:32 +0200

    added code for data validation

--- tech_basics_one/03Lecture/calculator.py
+++ tech_basics_one/03Lecture/calculator.py
@@ -8,19 +8,24 @@ num1 = input("Please enter the first number:")
 
 # convert the variables to float, otherwise Python will not recognise them as numbers
 # and if you want to make sure the user only enters a digit, you can add some logic that keeps looping until the user
-while num1.isdigit() !=True:
-    num1 = input("PLEASE ENTER A NUMBER:")
+while True:
+    try:
+        num1 = float(num1)
+        break
+    except:
+        num1 = input("PLEASE ENTER A NUMBER:")
 
-num1 = float(num1)
-
-operator = input("Please enter the operator (+, -, x, ➗):")
+operator = input("Please enter the operator (+, -, x, /):")
 num2 = input("Please enter the second number:")
 
 
-while num2.isdigit() !=True:
-    num2 = input("PLEASE ENTER A NUMBER:")
+while True:
+    try:
+        num2 = float(num2)
+        break
+    except:
+        num2 = input("PLEASE ENTER A NUMBER:")
 
-num2 = float(num2)
 
 # the if else-if satement that calculates what the user enters
 if operator == "+":
@@ -29,7 +34,7 @@ elif operator == "-":
     total = num1 - num2
 elif operator == "x":
     total = num1 * num2
-elif operator == "➗" and num2 > 0:
+elif operator == "/" and num2 > 0:
     total = num1 / num2
 else:
     total = "Math Error"
