a537a74898c90b5661cfde6f88165bf431ebd883
shaq31415926 <sarah.haq11@gmail.com>
2024-04-15 16:12:52 +0200

    code for class 3

---
+++ tech_basics_one/03Lecture/area.py
@@ -0,0 +1,6 @@
+# script to calculate the area of a rectangle
+length = float(input("What is the length? (cm):"))
+area = length ** 2
+
+# print("The area is", area, "cm2")
+print(f"The area is {area} cm2")
