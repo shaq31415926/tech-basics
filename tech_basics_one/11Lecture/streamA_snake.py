2198c7588f54acf787c25ec399c59dce8083a19b
shaq31415926 <sarah.haq11@gmail.com>
2024-06-11 12:50:28 +0200

    snake game live coding

--- tech_basics_one/11Lecture/streamA_snake.py
+++ tech_basics_one/11Lecture/streamA_snake.py
@@ -8,6 +8,9 @@ import random
 # 3. move the snake
 # 4. Added some logic to end the game when the snake dies
 # 5. Place the food
+# 6. Eat the food
+# 7. Display the score
+# 8. Increase the size of the snake
 
 # initialize pygame
 pg.init()
@@ -21,10 +24,12 @@ pg.display.set_caption("SNAKE")
 clock = pg.time.Clock()
 
 # create our snake
-snake_size = 20
-snake_length = 1
-snake = pg.Surface((snake_size, snake_size))  # define the width and height of the snake
-snake.fill((0, 0, 255))  # defining the colour
+snake_size = 20  # the length and width of the snake itself
+snake_length = 1  # how many squares you have
+snake_coordinates = [] # variable to keep track of the coordinates
+snake_colour = (0, 0, 255)
+#snake = pg.Surface((snake_size, snake_size))  # define the width and height of the snake
+#snake.fill((0, 0, 255))  # defining the colour
 # define the starting position
 x = 50
 y = 50
@@ -32,10 +37,16 @@ y = 50
 x1_change = 0
 y1_change = 0
 
+def display_snake(screen, snake_coordinates, snake_colour, snake_size):
+    for coordinates in snake_coordinates:
+        pg.draw.rect(screen, snake_colour, [coordinates[0], coordinates[1], snake_size, snake_size])
+
+
 # create our food
 # randomize the x and y coordinates and make sure it does not overlap the snake
-food_x = round(random.randrange(snake_size+10, disp_width - snake_size)/10)*10
-food_y = round(random.randrange(snake_size+10, disp_height - snake_size)/10)*10
+food_x = round(random.randrange(snake_size + 10, disp_width - snake_size) / 10) * 10
+food_y = round(random.randrange(snake_size + 10, disp_height - snake_size) / 10) * 10
+
 
 def display_message(display_text, screen, width, height):
     # set the font
@@ -44,9 +55,26 @@ def display_message(display_text, screen, width, height):
     text = font.render(display_text, True, (0, 255, 0), (0, 0, 0))
     # convert this into a rectangular object
     text_rect = text.get_rect()
-    text_rect.center = width/2, height/2
+    text_rect.center = width / 2, height / 2
     # place the message in the center of the screen
     screen.blit(text, text_rect)
+
+
+def display_score(display_text, screen, x, y):
+    # set the font
+    font = pg.font.SysFont("Papyrus", 20)
+    # create a text surface object, on which text is drawn on it. The last argument is the background colour.
+    text = font.render(display_text, True, (255, 255, 0), (0, 0, 0))
+    # place the message in the center of the screen
+    screen.blit(text, (x, y))
+
+def end_game(screen, disp_width, disp_height):
+    # CAN YOU ADD SOME MUSIC WHEN IT DIES?!
+    display_message("GAME OVER!!!!!!!!", screen, disp_width, disp_height)
+    pg.display.update()
+    time.sleep(4)
+    pg.quit()
+    quit()
 
 
 while True:
@@ -72,17 +100,27 @@ while True:
     # exit the game if x or y reaches the boundaries of the screen
     if x < 0 or y < 0 or x >= disp_width or y >= disp_height:
         # CAN YOU ADD SOME MUSIC WHEN IT DIES?!
-        display_message("GAME OVER!!!!!!!!", screen, disp_width, disp_height)
-        pg.display.update()
-        time.sleep(4)
-        pg.quit()
-        quit()
+        end_game(screen, disp_width, disp_height)
 
     # this is our game
     x += x1_change
     y += y1_change
     screen.fill((0, 0, 0))  # background colour
-    screen.blit(snake, (x, y))  # places the snake
+    #screen.blit(snake, (x, y))  # places the snake
+    # placing the snake
+    snake_head = [x, y]
+    snake_coordinates.append(snake_head)
+    display_snake(screen, snake_coordinates, snake_colour, snake_size)
+    # prevents the snake from placing another square each iteration
+    if len(snake_coordinates) > snake_length:
+        del snake_coordinates[0]
+
+    # end the game when the snake eats it tail
+    for coordinates in snake_coordinates[:-1]:
+        if coordinates == snake_head:
+            # CAN YOU ADD SOME MUSIC WHEN IT DIES?!
+            end_game(screen, disp_width, disp_height)
+
     # place the food
     apple_image = pg.image.load("images/Apple.png")
     screen.blit(apple_image, (food_x, food_y))
@@ -90,7 +128,6 @@ while True:
     # what happens when the snake eats the food
     if x == food_x and y == food_y:
         snake_length += 1
-        print(snake_length)
         # PLAY SOME MUSIC
         pg.mixer.music.load("music/explosion-6801.mp3")
         pg.mixer.music.play()
@@ -98,5 +135,6 @@ while True:
         food_x = round(random.randrange(snake_size + 10, disp_width - snake_size) / 10) * 10
         food_y = round(random.randrange(snake_size + 10, disp_height - snake_size) / 10) * 10
 
+    display_score(f"The score is: {snake_length - 1}", screen, 0, 0)
     pg.display.update()
     clock.tick(30)
