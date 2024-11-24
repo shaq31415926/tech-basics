f9a0ceaa388c06b8dcb530a1494cf30a1cff28cc
shaq31415926 <sarah.haq11@gmail.com>
2024-11-24 21:32:36 +0100

    mongo connection code

---
+++ tech_basics_two/07Lecture/mongo_connection.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import streamlit as st
+
+# load the user and db password from the secrets.toml file
+user = st.secrets['username']
+db_password = st.secrets['password']
+
+
+# This is our database connection string, for a cluster called tb-ii
+uri = f"mongodb+srv://{user}:{db_password}@tb-ii.guzgo.mongodb.net/?retryWrites=true&w=majority&appName=TB-II"
+
+# Let's connect to our MongoClient
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection, otherwise it will error
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
