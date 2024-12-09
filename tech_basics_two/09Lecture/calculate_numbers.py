47b7dcad491396a0092142b7f77e1371e3957c9e
shaq31415926 <sarah.haq11@gmail.com>
2024-12-09 20:25:31 +0100

    code to help calculate life path numbers

--- tech_basics_two/09Lecture/calculate_numbers.py
+++ tech_basics_two/09Lecture/calculate_numbers.py
@@ -1,10 +1,12 @@
-# These definitions have been generated with the help of ChatGPT
-def calculate_life_path_number(dob):
-    def reduce_to_single_digit(num):
-        while num > 9 and num not in [11, 22, 33]:
-            num = sum(int(digit) for digit in str(num))
-        return num
+# These functions have been generated with the help of ChatGPT
 
+def reduce_to_single_digit(num):
+    # Reduce to single digit or master number
+    while num > 9 and num not in [11, 22, 33]:
+        num = sum(int(digit) for digit in str(num))
+    return num
+
+def calculate_life_path_number(dob):
     # Extract day, month, and year
     year = dob.year
     month = dob.month
@@ -15,3 +17,8 @@ def calculate_life_path_number(dob):
 
     # Reduce to a single digit or master number
     return reduce_to_single_digit(total_sum)
+
+
+
+
+
