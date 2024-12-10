8439263ef14c918478135c4bbf9b3c6d9a0e5642
shaq31415926 <sarah.haq11@gmail.com>
2024-12-10 15:13:34 +0100

    code to connect to hugging face

---
+++ tech_basics_two/09Lecture/hugging_chat.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from hugchat.login import Login
+#from hugchat import hugchat
+
+st.title("Hugging Chat 💬🤗")
+
+
+@st.cache_resource
+def connect_to_hugging_face():
+    hf_email = st.secrets['email']
+    hf_pass = st.secrets['password']
+
+    # connect to hugging face
+    sign = Login(hf_email, hf_pass)
+    cookies = sign.login()
+
+    return cookies
+
+connect_to_hugging_face()
+
+
+
+
