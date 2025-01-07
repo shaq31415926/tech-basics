8f3ad768012ad754c5f2f86bfbc7a233a1b96540
shaq31415926 <sarah.haq11@gmail.com>
2025-01-07 14:29:55 +0100

    code from class

--- tech_basics_two/11Lecture/pet_data.py
+++ tech_basics_two/11Lecture/pet_data.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-tabs1, tabs2, tabs3 = st.tabs(["📊 Data", "Oldest Pet", "Youngest Pet"])
+# create tabs
+tabs1, tabs2, tabs3 = st.tabs(["📊 Data", "📈 Show Oldest Pet Data", "📈 Show Youngest Pet Data"])
+
+# read data
+data = pd.read_csv("data/sample_data.csv")
 
 with tabs1:
-    data = pd.read_csv("data/sample_data.csv")
     st.write(data)
 
 with tabs2:
-    #st.line_chart(data['age'])
+    # get the oldest pet name and display image
     oldest_pet = list(data[data['age'] == data['age'].max()]['pet_name'])[0]
-    st.title(f"Image of {oldest_pet}")
+    st.title(f"This is the page for {oldest_pet}")
     st.image(list(data[data['age'] == data['age'].max()]['image_url'])[0])
 
 with tabs3:
+    # get the youngest pet name and display image
     youngest_pet = list(data[data['age'] == data['age'].min()]['pet_name'])[0]
-    st.title(f"Image of {youngest_pet}")
+    st.title(f"This is the page for {youngest_pet}")
     st.image(list(data[data['age'] == data['age'].min()]['image_url'])[0])
-
-
